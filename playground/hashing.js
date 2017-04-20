af4433981cfd7d1e826c9bd384164ec3700e4b85
glstream <grayson.stream@gmail.com>
2017-04-20 14:57:48 -0700

    a

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,18 +1,35 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
-
-var data = {
-    id:10
-};
+const bcrypt = require('bcryptjs');
 
 
-var token = jwt.sign(data, '123abc');
+var password = '123abc!';
+
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash)
+    })
+})
+
+var hashedPassword = '$2a$10$oTW2ZTXIikVn7uj36ObUVu4ISUZKWe3EhUhlTLiYwWjhqY2L7vefK'
 
 
-var decoded = jwt.verify(token, '123abc');
+bcrypt.compare(password, hashedPassword, (err, result) => {
+    console.log(result);
+})
 
-console.log(token);
-console.log(`decoded: ${JSON.stringify(decoded).toString()}`)
+
+// var data = {
+//     id:10
+// };
+
+
+// var token = jwt.sign(data, '123abc');
+
+
+// var decoded = jwt.verify(token, '123abc');
+
+// console.log(token);
 // var message = 'I am user number 3';
 
 // var hash = SHA256(message).toString();
